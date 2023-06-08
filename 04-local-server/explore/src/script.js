d781c94acf2c9b5891091bf4d209829f549d1945
skcajs <sjackson864@outlook.com>
2023-06-08 16:02:38 +0200

    completed lesson 04

--- 04-local-server/explore/src/script.js
+++ 04-local-server/explore/src/script.js
@@ -4,8 +4,8 @@ import * as THREE from 'three'
 const scene = new THREE.Scene()
 
 // Red Cube
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const geometry = new THREE.ConeGeometry( 5, 20, 32 );
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
@@ -17,7 +17,7 @@ const size = {
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, size.width / size.height)
-camera.position.z = 3
+camera.position.z = 30
 scene.add(camera)
 
 // Renderer
